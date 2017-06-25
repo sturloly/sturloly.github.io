b873f507521f38f7714a18f003c67812f27da9ba
sturloly <sturloly@gmail.com>
2017-06-25 23:54:13 +0800

    pug ini

--- node-express-pug/app.js
+++ node-express-pug/app.js
@@ -1,9 +1,16 @@
 const express = require('express');
+const path = require('path');  // core module inclued by default
 
+// init app
 const app = express();
 
+// load view engine
+app.set('views', path.join(__dirname, 'views'));  // dirname meaning the current direc, view -- views folder
+app.set('view engine', 'pug'); // chose the pug view engine
+
+// home route
 app.get('/', function(req, res){
-  res.send('stop hello world please');
+  res.render('index');
 });
 
 app.listen(3000,function(){

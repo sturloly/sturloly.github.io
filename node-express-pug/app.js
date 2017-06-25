1279a45adb6def809603fa41b771f6d42334fea5
sturloly <sturloly@gmail.com>
2017-06-26 00:29:21 +0800

    multi route

--- node-express-pug/app.js
+++ node-express-pug/app.js
@@ -10,9 +10,20 @@ app.set('view engine', 'pug'); // chose the pug view engine
 
 // home route
 app.get('/', function(req, res){
-  res.render('index');
+  res.render('index', {
+    title:'Hello World'
+  });
 });
 
+// add route
+app.get('/articles/add' , function(req, res){
+  res.render('add_article', {
+    title:'Add Articles'
+  })
+});
+
+
+//start server
 app.listen(3000,function(){
   console.log("Server Started On Port 3000....")
 });
